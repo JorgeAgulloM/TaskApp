771cad4c2e8ddab44e5012858ad6b1c6e0da06e1
Jorge Agulló <agullojorge@gmail.com>
2022-08-08 09:21:34 +0200

    [WIP] Adding ROOM DB, add TaskViewModel.kt

---
+++ app/src/main/java/com/softyorch/taskapp/screens/main/TaskViewModel.kt
@@ -0,0 +1,36 @@
+package com.softyorch.taskapp.screens.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.softyorch.taskapp.model.Task
+import com.softyorch.taskapp.repository.TaskRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class TaskViewModel @Inject constructor(private val repository: TaskRepository) : ViewModel() {
+    private val _taskList = MutableStateFlow<List<Task>>(emptyList())
+    val taskList = _taskList.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAllTasks().distinctUntilChanged()
+                .collect { listOfTasks ->
+                    if (listOfTasks.isNullOrEmpty()) {
+                        //TODO meter la barra de carga para mostrar al usuario
+                    } else {
+                        _taskList.value = listOfTasks
+                    }
+                }
+        }
+    }
+
+    fun addTask(task: Task) = viewModelScope.launch { repository.addTask(task = task) }
+    fun updateTask(task: Task) = viewModelScope.launch { repository.updateTask(task = task) }
+    fun removeTask(task: Task) = viewModelScope.launch { repository.deleteTask(task = task) }
+
+}
