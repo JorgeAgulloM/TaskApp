65a4584f4bde9d4d48d3a134d790c5af4aaabe3c
Jorge Agulló <agullojorge@gmail.com>
2022-08-08 08:58:41 +0200

    [WIP] Adding ROOM DB, add AppModule.kt

--- app/src/main/java/com/softyorch/taskapp/di/AppModule.kt
+++ app/src/main/java/com/softyorch/taskapp/di/AppModule.kt
@@ -1,10 +1,31 @@
 package com.softyorch.taskapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.softyorch.taskapp.data.TaskDatabase
+import com.softyorch.taskapp.data.TaskDatabaseDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTaskDao(taskDatabase: TaskDatabase): TaskDatabaseDao = taskDatabase.taskDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): TaskDatabase =
+        Room.databaseBuilder(
+            context,
+            TaskDatabase::class.java,
+            "tasks_tbl")
+            .fallbackToDestructiveMigration()
+            .build()
 }
