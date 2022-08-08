322750ce9bf452bbc529fac86c0731a83f68c3e8
Jorge Agulló <agullojorge@gmail.com>
2022-08-08 10:04:25 +0200

    [WIP] Adding ROOM DB, add Converters Utilities

---
+++ app/src/main/java/com/softyorch/taskapp/utils/UUIDConverter.kt
@@ -0,0 +1,12 @@
+package com.softyorch.taskapp.utils
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class UUIDConverter {
+    @TypeConverter
+    fun fromUUID(uuid: UUID): String? = uuid.toString()
+
+    @TypeConverter
+    fun uuidFromString(string: String): UUID? = UUID.fromString(string)
+}
