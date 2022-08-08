d966ab5c9718d741112ab49bad8dd6039bb8526a
Jorge Agulló <agullojorge@gmail.com>
2022-08-08 08:35:45 +0200

    [WIP] Adding ROOM DB, add Task.kt

---
+++ app/src/main/java/com/softyorch/taskapp/model/Task.kt
@@ -0,0 +1,22 @@
+package com.softyorch.taskapp.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
+import java.util.*
+
+@Entity(tableName = "tasks_tbl")
+data class Task(
+    @PrimaryKey
+    val id: UUID = UUID.randomUUID(),
+
+    @ColumnInfo(name = "task_title")
+    val title: String,
+
+    @ColumnInfo(name = "task_description")
+    val description: String,
+
+    @ColumnInfo(name = "task_entry_date")
+    val entryDate: Date = Date.from(Instant.now()),
+)
