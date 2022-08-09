226bc58077d9ceaa1f65ecc9e608c7718c3e40be
Jorge Agulló <agullojorge@gmail.com>
2022-08-09 19:28:54 +0200

    [WIP] History $ delete

--- app/src/main/java/com/softyorch/taskapp/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/softyorch/taskapp/screens/detail/DetailScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.softyorch.taskapp.data.Resource
@@ -121,7 +122,8 @@ private fun Content(
                                     }, "Cancel")
                                 },
                                 title = { Text(text = "Eliminar tarea") },
-                                text = { Text(text = "¿Estas seguro de eliminar esta tarea?") })
+                                text = { Text(text = "¿Estas seguro de eliminar esta tarea?") }
+                            )
                         }
                     }
                 }
