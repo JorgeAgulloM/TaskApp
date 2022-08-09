6883a64f1b34d554c5f623e8302c312aecfe6645
Jorge Agulló <agullojorge@gmail.com>
2022-08-09 18:24:35 +0200

    [WIP] History

--- app/src/main/java/com/softyorch/taskapp/screens/history/HistoryScreen.kt
+++ app/src/main/java/com/softyorch/taskapp/screens/history/HistoryScreen.kt
@@ -1,31 +1,32 @@
 package com.softyorch.taskapp.screens.history
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.History
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.softyorch.taskapp.navigation.TaskAppScreens
 import com.softyorch.taskapp.screens.main.TaskViewModel
-import com.softyorch.taskapp.utils.Hello
-import com.softyorch.taskapp.utils.RowIndication
 import com.softyorch.taskapp.utils.TopAppBar
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HistoryScreen(navController: NavHostController, taskViewModel: TaskViewModel) {
-    Hello("History")
     Scaffold(
         topBar = {
             TopAppBar(
@@ -50,15 +51,31 @@ private fun Content(
 
     LazyColumn(
         modifier = Modifier.fillMaxSize()
-            .padding(top = it.calculateTopPadding(), start = 8.dp, end = 8.dp)
+            .padding(top = it.calculateTopPadding() * 1.5f, start = 16.dp, end = 16.dp)
     ) {
         items(tasks) { task ->
             Row(
+                modifier = Modifier.clickable {
+                    navController.navigate(TaskAppScreens.DetailsScreen.name + "/${task.id}")
+                },
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
+                horizontalArrangement = Arrangement.Start
             ) {
-                Text(text = task.entryDate.toString())
-                Text(text = task.title)
+                Text(
+                    modifier = Modifier.padding(end = 8.dp),
+                    text = task.entryDate.toInstant().toString().split("T")[0],
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = 12.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+                Text(
+                    text = task.title,
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontSize = 14.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
             }
         }
     }
