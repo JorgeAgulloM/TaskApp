65c09bc43c0790e16ccccd80591475259edcd64b
Jorge Agulló <agullojorge@gmail.com>
2022-08-18 08:01:26 +0200

    [WIP] change version of DB

--- app/src/main/java/com/softyorch/taskapp/data/userdata/UserDataBase.kt
+++ app/src/main/java/com/softyorch/taskapp/data/userdata/UserDataBase.kt
@@ -4,10 +4,11 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.softyorch.taskapp.model.UserData
+import com.softyorch.taskapp.utils.DateConverter
 import com.softyorch.taskapp.utils.UUIDConverter
 
-@Database(entities = [UserData::class], version = 1, exportSchema = false)
-@TypeConverters(UUIDConverter::class)
+@Database(entities = [UserData::class], version = 3, exportSchema = false)
+@TypeConverters(DateConverter::class, UUIDConverter::class)
 abstract class UserDataBase: RoomDatabase() {
     abstract fun userDataDao(): UserDataBaseDao
 }
