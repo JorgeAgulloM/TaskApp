e9b71f831b22de8ddec48baf496524abf53ab309
Jorge Agulló <agullojorge@gmail.com>
2022-08-19 17:55:13 +0200

    [WIP] new logic for use AutoLogin Hilt Injection

--- app/src/main/java/com/softyorch/taskapp/screens/splash/SplashViewModel.kt
+++ app/src/main/java/com/softyorch/taskapp/screens/splash/SplashViewModel.kt
@@ -1,14 +1,50 @@
 package com.softyorch.taskapp.screens.splash
 
+import android.content.SharedPreferences
+import androidx.compose.runtime.produceState
 import androidx.lifecycle.ViewModel
 import com.softyorch.taskapp.data.Resource
 import com.softyorch.taskapp.model.UserData
 import com.softyorch.taskapp.repository.UserDataRepository
+import com.softyorch.taskapp.utils.login.AutoLogin
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.Instant
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
-class SplashViewModel @Inject constructor(private val repository: UserDataRepository): ViewModel() {
+class SplashViewModel @Inject constructor(
+    private val repository: UserDataRepository,
+    private val autoLogin: AutoLogin
+) : ViewModel() {
     suspend fun logInWithRememberMe(name: String, pass: String): Resource<UserData> =
         repository.signInSharePreferences(name = name, password = pass)
+
+    fun loadSharedPreferencesInAutoLogin(sharedPreferences: SharedPreferences) {
+        autoLogin.loadSharedPreferences(sharedPreferences = sharedPreferences)
+    }
+
+    suspend fun userActivated(): Boolean {
+        if (autoLogin.isTheUserActive() == true) {
+            autoLogin.userActive().let { userData ->
+                logInWithRememberMe(
+                    name = userData.username,
+                    pass = userData.userPass
+                ).let { data ->
+                    data.data?.let { user ->
+                        if (user.rememberMe == true) {
+                            val timeWeekInMillis = 604800000
+                            user.lastLoginDate?.time?.let { timeDiff ->
+                                Date.from(Instant.now()).time.minus(timeDiff)
+                                timeDiff.compareTo(timeWeekInMillis).let {
+                                    return true
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false
+    }
 }
