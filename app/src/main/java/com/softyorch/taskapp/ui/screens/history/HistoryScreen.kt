fe359f445e34f7ef04df65ae686427b367c11e6c
Jorge Agulló <agullojorge@gmail.com>
2022-08-28 08:43:19 +0200

    [WIP] refactor

--- app/src/main/java/com/softyorch/taskapp/ui/screens/history/HistoryScreen.kt
+++ app/src/main/java/com/softyorch/taskapp/ui/screens/history/HistoryScreen.kt
@@ -9,15 +9,19 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.softyorch.taskapp.model.Task
 import com.softyorch.taskapp.ui.components.topAppBarCustom.TopAppBarCustom
 import com.softyorch.taskapp.navigation.AppScreens
+import com.softyorch.taskapp.utils.StandardizedSizes
 import com.softyorch.taskapp.utils.toStringFormatDate
 
 
@@ -46,14 +50,15 @@ private fun Content(
     navController: NavHostController,
     viewModel: HistoryViewModel
 ) {
-    val tasks = viewModel.taskList.collectAsState().value
+    //val tasks = viewModel.taskList.collectAsState().value
+    val tasks: State<List<Task>> = viewModel.taskList.observeAsState(initial = emptyList())
     val textSizes = viewModel.sizeSelectedOfUser()
 
     LazyColumn(
         modifier = Modifier.fillMaxSize()
             .padding(top = it.calculateTopPadding() * 1.5f, start = 16.dp, end = 16.dp)
     ) {
-        items(tasks) { task ->
+        items(tasks.value) { task ->
             Row(
                 modifier = Modifier
                     .padding(top = 4.dp)
@@ -63,22 +68,38 @@ private fun Content(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Start
             ) {
-                Text(
-                    modifier = Modifier.padding(end = 8.dp),
-                    text = task.entryDate.toStringFormatDate(),
-                    color = MaterialTheme.colorScheme.onSurface,
-                    fontSize = textSizes.minimumSize,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
-                )
-                Text(
-                    text = task.title,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    fontSize = textSizes.normalSize,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
-                )
+                TextHeadHistry(task, textSizes)
+                TextContentHistory(task, textSizes)
             }
         }
     }
+}
+
+@Composable
+private fun TextHeadHistry(
+    task: Task,
+    textSizes: StandardizedSizes
+) {
+    Text(
+        modifier = Modifier.padding(end = 8.dp),
+        text = task.entryDate.toStringFormatDate(),
+        color = MaterialTheme.colorScheme.onSurface,
+        fontSize = textSizes.minimumSize,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1
+    )
+}
+
+@Composable
+private fun TextContentHistory(
+    task: Task,
+    textSizes: StandardizedSizes
+) {
+    Text(
+        text = task.title,
+        color = MaterialTheme.colorScheme.onSurface,
+        fontSize = textSizes.normalSize,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1
+    )
 }
--- app/src/main/java/com/softyorch/taskapp/ui/screens/history/HistoryViewModel.kt
+++ app/src/main/java/com/softyorch/taskapp/ui/screens/history/HistoryViewModel.kt
@@ -1,5 +1,7 @@
 package com.softyorch.taskapp.ui.screens.history
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.softyorch.taskapp.model.Task
@@ -19,17 +21,17 @@ class HistoryViewModel @Inject constructor(
     private val repository: TaskRepository,
     private val stateLogin: StateLogin
 ) : ViewModel() {
-    private val _taskList = MutableStateFlow<List<Task>>(emptyList())
-    val taskList = _taskList.asStateFlow()
+    private val _taskList = MutableLiveData<List<Task>>()
+    val taskList: LiveData<List<Task>> = _taskList
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
             repository.getAllTasks().distinctUntilChanged()
                 .collect { listOfTasks ->
-                    if (listOfTasks.isNullOrEmpty()) {
+                    if (listOfTasks.isEmpty()) {
                         //TODO meter la barra de carga para mostrar al usuario
                     } else {
-                        _taskList.value = listOfTasks
+                        _taskList.postValue(listOfTasks)
                     }
                 }
         }
