ead44fcbe5ffd3ed0e251cb48efad7338581f752
Jorge Agulló <agullojorge@gmail.com>
2022-09-06 13:28:32 +0200

    [UPDATE] Added Material 3 colors palette with custom color (006783)

--- app/src/main/java/com/softyorch/taskapp/presentation/theme/Color.kt
+++ app/src/main/java/com/softyorch/taskapp/presentation/theme/Color.kt
@@ -16,3 +16,65 @@ val ReplyBlue400 = Color(0xFF97B0B0)
 val ReplyOrange500 = Color(0xFFF9AA33)
 val DarkMode90t = Color(0xFF232F34)
 val LightMode90t = Color(0xE6FFFFFF)
+
+/** Light Colors*/
+val AppPrimaryLight = Color(0xFF65D3FF)
+val AppOnPrimaryLight = Color(0xFF003546)
+val AppPrimaryContainerLight = Color(0xFF004D64)
+val AppOnPrimaryContainerLight = Color(0xFFBDE9FF)
+
+val AppSecondaryLight = Color(0xFFB2CAD7)
+val AppOnSecondaryLight = Color(0xFF1D333D)
+val AppSecondaryContainerLight = Color(0xFF344A54)
+val AppOnSecondaryContainerLight = Color(0xFFCEE6F3)
+
+val AppTertiaryLight = Color(0xFFC5C2ED)
+val AppOnTertiaryLight = Color(0xFF2E2D4F)
+val AppTertiaryContainerLight = Color(0xFF454367)
+val AppOnTertiaryContainerLight = Color(0xFFE3DFFF)
+
+val AppErrorLight = Color(0xFFFFB4AB)
+val AppOnErrorLight = Color(0xFF690005)
+val AppErrorContainerLight = Color(0xFF93000A)
+val AppOnErrorContainerLight = Color(0xFFFFB4AB)
+
+val AppBackgroundLight = Color(0xFF191C1E)
+val AppOnBackgroundLight = Color(0xFFE1E2E4)
+
+val AppSurfaceLight = Color(0xFF191C1E)
+val AppOnSurfaceLight = Color(0xFFE1E2E4)
+val AppSurfaceVariantLight = Color(0xFF40484C)
+val AppOnSurfaceVariantLight = Color(0xFFC0C8CD)
+
+val AppOutLineLight = Color(0xFF8A9297)
+
+/** Dark Colors*/
+val AppPrimaryDark = Color(0xFF006783)
+val AppOnPrimaryDark = Color(0xFFFFFFFF)
+val AppPrimaryContainerDark = Color(0xFFBDE9FF)
+val AppOnPrimaryContainerDark = Color(0xFF001F2A)
+
+val AppSecondaryDark = Color(0xFF4B626C)
+val AppOnSecondaryDark = Color(0xFFFFFFFF)
+val AppSecondaryContainerDark = Color(0xFFCEE6F3)
+val AppOnSecondaryContainerDark = Color(0xFF061E28)
+
+val AppTertiaryDark = Color(0xFF5C5B80)
+val AppOnTertiaryDark = Color(0xFFFFFFFF)
+val AppTertiaryContainerDark = Color(0xFFE3DFFF)
+val AppOnTertiaryContainerDark = Color(0xFF191739)
+
+val AppErrorDark = Color(0xFFBA1A1A)
+val AppOnErrorDark = Color(0xFFFFFFFF)
+val AppErrorContainerDark = Color(0xFFFFDAD6)
+val AppOnErrorContainerDark = Color(0xFF410002)
+
+val AppBackgroundDark  = Color(0xFFFBFCFE)
+val AppOnBackgroundDark  = Color(0xFF191C1E)
+
+val AppSurfaceDark  = Color(0xFFFBFCFE)
+val AppOnSurfaceDark  = Color(0xFF191C1E)
+val AppSurfaceVariantDark  = Color(0xFFDCE4E9)
+val AppOnSurfaceVariantDark  = Color(0xFF40484C)
+
+val AppOutLineDark = Color(0xFF70787D)
