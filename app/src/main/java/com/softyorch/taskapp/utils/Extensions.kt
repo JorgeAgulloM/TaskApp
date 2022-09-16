578846f3961c3b2a922d3105be7c48f0ef82ab39
Jorge Agulló <agullojorge@gmail.com>
2022-09-16 18:20:29 +0200

    [WIP] Animations: Animation code has been added to Booleans

--- app/src/main/java/com/softyorch/taskapp/utils/Extensions.kt
+++ app/src/main/java/com/softyorch/taskapp/utils/Extensions.kt
@@ -3,7 +3,19 @@ package com.softyorch.taskapp.utils
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import android.view.PixelCopy
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntOffsetAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.datastore.preferences.preferencesDataStore
 import java.text.SimpleDateFormat
 import java.time.Instant
@@ -33,3 +45,39 @@ val Context.datastore by preferencesDataStore(name = USER_DATA)
 fun Context.toastError(message: String, onShow: () -> Unit) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show().also { onShow() }
 }
+
+/** Animations ***************************************************/
+
+@Composable
+fun Boolean.alphaAnimation(): State<Float> =
+    animateFloatAsState(
+        targetValue = if (this) 0.2f else 1f,
+        animationSpec = tween(
+            durationMillis = 200,
+            delayMillis = DURATION_MILLIS_BTN_CHANGE_COLOR,
+            easing = FastOutSlowInEasing
+        )
+    )
+
+@Composable
+fun Boolean.containerColorAnimation(): State<Color> =
+    animateColorAsState(
+        targetValue =
+        if (this) MaterialTheme.colorScheme.primaryContainer
+        else MaterialTheme.colorScheme.background,
+        animationSpec = tween(
+            durationMillis = DURATION_MILLIS_BTN_CHANGE_COLOR,
+            easing = FastOutSlowInEasing)
+    )
+
+@Composable
+fun Boolean.intOffsetAnimation(finishedListener: () -> Unit?): State<IntOffset> = animateIntOffsetAsState(
+    targetValue = if (this) IntOffset(0, 0)
+    else IntOffset(1500, 0),
+    animationSpec = tween(
+        durationMillis = 400,
+        delayMillis = DURATION_MILLIS_BTN_CHANGE_COLOR,
+        easing = FastOutSlowInEasing
+    ),
+    finishedListener = { finishedListener() }
+)
