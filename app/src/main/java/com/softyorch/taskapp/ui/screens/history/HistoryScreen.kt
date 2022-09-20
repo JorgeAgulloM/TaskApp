988da725c55c5bc9ea29d6146f70735c656f4f3b
Jorge Agulló <agullojorge@gmail.com>
2022-09-20 15:08:00 +0200

    [WIP] Add background to HistoryScreen.kt

--- app/src/main/java/com/softyorch/taskapp/ui/screens/history/HistoryScreen.kt
+++ app/src/main/java/com/softyorch/taskapp/ui/screens/history/HistoryScreen.kt
@@ -1,29 +1,35 @@
 package com.softyorch.taskapp.ui.screens.history
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.softyorch.taskapp.R
 import com.softyorch.taskapp.ui.components.topAppBarCustom.TopAppBarCustom
 import com.softyorch.taskapp.ui.navigation.AppScreens
 import com.softyorch.taskapp.R.string.history
 import com.softyorch.taskapp.data.database.tasks.TaskEntity
+import com.softyorch.taskapp.ui.navigation.AppScreensRoutes
 import com.softyorch.taskapp.utils.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CoroutineCreationDuringComposition")
 @ExperimentalMaterial3Api
@@ -31,23 +37,62 @@ import com.softyorch.taskapp.utils.*
 fun HistoryScreen(navController: NavHostController) {
 
     val viewModel = hiltViewModel<HistoryViewModel>()
+    val coroutineScope = rememberCoroutineScope()
+    var enterDetails by remember { mutableStateOf(value = false) }
+    var exitDetails by remember { mutableStateOf(value = false) }
 
-    Scaffold(
-        topBar = {
-            TopAppBarCustom(
-                title = stringResource(history),
-                nameScreen = AppScreens.HistoryScreen.name,
-                navController = navController,
-            )
-        }
+    coroutineScope.launch {
+        //viewModel.getTask(id = id)
+        delay(100)
+        if (!exitDetails) enterDetails = true
+    }
+
+    val slideCheckBox by enterDetails.intOffsetAnimation {
+        if (!enterDetails)
+            navController.navigate(AppScreensRoutes.MainScreen.route) {
+                popUpTo(AppScreensRoutes.DetailScreen.route) {
+                    inclusive = true
+                    navController.backQueue.clear()
+                }
+            }
+    }
+    val alphaAnimation: Float by exitDetails.alphaAnimation()
+    val colorAnimation by exitDetails.containerColorAnimation()
+    Column(
+        modifier = Modifier
+            .background(color = MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.5f))
+            //.padding(8.dp)
+            .offset { slideCheckBox }
+            .graphicsLayer(alpha = alphaAnimation)
     ) {
-        Content(it = it, navController = navController, viewModel = viewModel)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            IconButton(
+                onClick = {
+                    exitDetails = true
+                    enterDetails = false
+                },
+                modifier = Modifier
+                    .padding(8.dp)
+                    .background(color = colorAnimation, shape = MaterialTheme.shapes.large),
+                //colors = IconButtonDefaults.iconButtonColors(containerColor = containerColor)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = stringResource(
+                        R.string.go_to_home
+                    )
+                )
+            }
+        }
+        Content(navController = navController, viewModel = viewModel)
     }
 }
 
 @Composable
 private fun Content(
-    it: PaddingValues,
     navController: NavHostController,
     viewModel: HistoryViewModel
 ) {
@@ -59,7 +104,13 @@ private fun Content(
     if (error) LocalContext.current.toastError(messageError) { viewModel.errorShown() }
     LazyColumn(
         modifier = Modifier.fillMaxSize()
-            .padding(top = it.calculateTopPadding() * 1.5f, start = 16.dp, end = 16.dp)
+            .padding(8.dp)
+            .shadow(elevation = ELEVATION_DP, shape = MaterialTheme.shapes.large)
+            .background(
+                color = MaterialTheme.colorScheme.background,
+                shape = MaterialTheme.shapes.large
+            )
+
     ) {
         items(taskEntities) { task ->
             Row(
@@ -83,7 +134,7 @@ private fun TextHeadHistry(
     taskEntity: TaskEntity
 ) {
     Text(
-        modifier = Modifier.padding(end = 8.dp),
+        modifier = Modifier.padding(horizontal = 8.dp),
         text = taskEntity.entryDate.toStringFormatDate(),
         color = MaterialTheme.colorScheme.onSurface,
         style = MaterialTheme.typography.bodyLarge,
