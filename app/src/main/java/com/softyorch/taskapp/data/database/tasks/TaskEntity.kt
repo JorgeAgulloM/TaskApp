2f102f7e2663012ee7546512e991b96519cd85c7
Jorge Agulló <agullojorge@gmail.com>
2022-09-20 05:56:43 +0200

    [UPDATE] Refactor: Names of DBs has been moved in DBs companion objects

--- app/src/main/java/com/softyorch/taskapp/data/database/tasks/TaskEntity.kt
+++ app/src/main/java/com/softyorch/taskapp/data/database/tasks/TaskEntity.kt
@@ -3,11 +3,12 @@ package com.softyorch.taskapp.data.database.tasks
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.softyorch.taskapp.data.database.tasks.TaskDatabase.Companion.TASK_DB_NAME
 import com.softyorch.taskapp.utils.*
 import java.time.Instant
 import java.util.*
 
-@Entity(tableName = TASK_TBL)
+@Entity(tableName = TASK_DB_NAME)
 data class TaskEntity(
     @PrimaryKey val id: UUID = UUID.randomUUID(),
     @ColumnInfo(name = TITLE) var title: String,
