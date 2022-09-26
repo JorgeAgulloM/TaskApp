706a1e1c73c9d26282cd20be17d22dff107f9bb5
Jorge Agulló <agullojorge@gmail.com>
2022-09-26 09:47:03 +0200

    [WIP] Added ascending or descending case

--- app/src/main/java/com/softyorch/taskapp/domain/taskUsesCase/GetAllTask.kt
+++ app/src/main/java/com/softyorch/taskapp/domain/taskUsesCase/GetAllTask.kt
@@ -13,21 +13,26 @@ class GetAllTask(private val repository: TaskRepository) {
         return repository.getAllTaskFromDatabase().map { task ->
             when (taskOrder.orderType) {
                 is OrderType.Ascending -> {
-                    when (taskOrder){
+                    when (taskOrder) {
                         is TaskOrder.Create -> task.sortedBy { it.entryDate }
                         is TaskOrder.Name -> task.sortedBy { it.title.lowercase() }
                         is TaskOrder.Finish -> task.sortedBy { it.finishDate }
                     }
                 }
                 is OrderType.Descending -> {
-                    when (taskOrder){
+                    when (taskOrder) {
                         is TaskOrder.Create -> task.sortedByDescending { it.entryDate }
                         is TaskOrder.Name -> task.sortedByDescending { it.title.lowercase() }
                         is TaskOrder.Finish -> task.sortedByDescending { it.finishDate }
                     }
                 }
             }
-        }.map { it -> it.sortedBy { it.checkState } }
+        }.map { it ->
+            when (taskOrder.orderType) {
+                OrderType.Ascending -> it.sortedBy { it.checkState }
+                OrderType.Descending -> it.sortedByDescending { it.checkState }
+            }
+        }
     }
 }
 
