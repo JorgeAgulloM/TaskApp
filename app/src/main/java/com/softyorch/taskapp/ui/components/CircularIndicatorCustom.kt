d3273a8ee428a6a467d5f480d1d80375ffa9b04e
Jorge Agulló <agullojorge@gmail.com>
2022-09-27 07:24:18 +0200

    [WIP] Background added to CircularProgressIndicator

--- app/src/main/java/com/softyorch/taskapp/ui/components/CircularIndicatorCustom.kt
+++ app/src/main/java/com/softyorch/taskapp/ui/components/CircularIndicatorCustom.kt
@@ -1,22 +1,26 @@
 package com.softyorch.taskapp.ui.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
+import com.softyorch.taskapp.utils.ELEVATION_DP
 import com.softyorch.taskapp.utils.infiniteTransitionAnimateColor
 
 @Composable
 fun CircularIndicatorCustomDialog(
     text: String,
     modifier: Modifier = Modifier
-){
+) {
     Dialog(
         onDismissRequest = {},
         properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
@@ -29,16 +33,31 @@ fun CircularIndicatorCustomDialog(
 fun CircularIndicatorCustom(
     text: String,
     modifier: Modifier = Modifier
-){
+) {
     Column(
-        modifier = modifier,
+        modifier = modifier.safeContentPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        CircularProgressIndicator(
-            color = MaterialTheme.infiniteTransitionAnimateColor().value
+        Box(
+            modifier = modifier
+                .size(45.dp)
+                .shadow(elevation = ELEVATION_DP * 2, shape = CircleShape)
+                .background(
+                    color = MaterialTheme.colorScheme.background,
+                    shape = CircleShape
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.infiniteTransitionAnimateColor().value
+            )
+        }
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.surface
         )
-        Text(text = text, style = MaterialTheme.typography.bodyMedium)
     }
 
 }
