c00b1bb6e0b6d8dd70a99d8a1e855acce53d464c
Jorge Agulló <agullojorge@gmail.com>
2022-09-28 18:55:40 +0200

    [WIP] Added use case Tests

---
+++ app/src/test/java/com/softyorch/taskapp/domain/userdataUseCase/GetUserEmailExistTest.kt
@@ -0,0 +1,67 @@
+package com.softyorch.taskapp.domain.userdataUseCase
+
+import com.softyorch.taskapp.data.database.userdata.UserDataEntity
+import com.softyorch.taskapp.data.repository.UserDataRepository
+import com.softyorch.taskapp.utils.emptyString
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetUserEmailExistTest {
+
+    @RelaxedMockK
+    private lateinit var repository: UserDataRepository
+
+    lateinit var getUserEmailExist: GetUserEmailExist
+
+    @Before
+    fun onBefore() {
+        MockKAnnotations.init(this)
+        getUserEmailExist = GetUserEmailExist(repository = repository)
+    }
+
+    @Test
+    fun `cuando se solicita un email que no existe`() = runBlocking {
+
+        val user = UserDataEntity(
+            username = emptyString,
+            userEmail = emptyString,
+            userPass = emptyString,
+            userPicture = emptyString
+        )
+
+        //Given
+        coEvery { repository.getUserDataEmail(email = user.userEmail) } returns null
+
+        //When
+        getUserEmailExist.invoke(email = user.userEmail)
+
+        //Then
+        coVerify(exactly = 1) { repository.getUserDataEmail(any()) }
+    }
+
+    @Test
+    fun `cuando se solicita un email que si existe`() = runBlocking {
+
+        val user = UserDataEntity(
+            username = "Jorge",
+            userEmail = "jorge@mail.com",
+            userPass = emptyString,
+            userPicture = emptyString
+        )
+
+        //Given
+        coEvery { repository.getUserDataEmail(email = user.userEmail) } returns user
+
+        //When
+        val getUser = getUserEmailExist.invoke(email = user.userEmail)
+
+        //Then
+        coVerify(exactly = 1) { repository.getUserDataEmail(any()) }
+        assert(user == getUser)
+    }
+}
