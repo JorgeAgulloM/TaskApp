8bde2863da865175fdd73660e3536e19bd31dae9
Jorge Agulló <agullojorge@gmail.com>
2022-09-30 08:12:40 +0200

    [WIP] Added use case test  - Working

--- app/src/test/java/com/softyorch/taskapp/domain/taskUsesCase/GetTaskIdTest.kt
+++ app/src/test/java/com/softyorch/taskapp/domain/taskUsesCase/GetTaskIdTest.kt
@@ -1,6 +1,5 @@
 package com.softyorch.taskapp.domain.taskUsesCase
 
-import com.softyorch.taskapp.data.Resource
 import com.softyorch.taskapp.data.database.tasks.TaskEntity
 import com.softyorch.taskapp.data.repository.TaskRepository
 import io.mockk.MockKAnnotations
