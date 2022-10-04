67a3fe8735320be4b43d1fe359d8d28b7513436c
Jorge Agulló <agullojorge@gmail.com>
2022-10-04 14:28:49 +0200

    [WIP] Switches to a url with a customized photo collection

--- app/src/main/java/com/softyorch/taskapp/domain/pexelUseCase/GetImage.kt
+++ app/src/main/java/com/softyorch/taskapp/domain/pexelUseCase/GetImage.kt
@@ -1,10 +1,10 @@
 package com.softyorch.taskapp.domain.pexelUseCase
 
-import com.softyorch.taskapp.data.network.pexels.response.Photo
+import com.softyorch.taskapp.data.network.pexels.responseMyCollection.Media
 import com.softyorch.taskapp.data.repository.PexelsRepository
 
 class GetImage(private val repository: PexelsRepository) {
 
-    suspend operator fun invoke(queryParam: String): Photo =
-        repository.getRandomImage(queryParam)
+    suspend operator fun invoke(): Media =
+        repository.getRandomImage()
 }
