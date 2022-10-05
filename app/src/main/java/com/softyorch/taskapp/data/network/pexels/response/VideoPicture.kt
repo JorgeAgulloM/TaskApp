ed4e727bb03034ffdcd81b893308ef0163dcda39
Jorge Agulló <agullojorge@gmail.com>
2022-10-05 11:53:00 +0200

    [WIP] @SerializedName added

--- app/src/main/java/com/softyorch/taskapp/data/network/pexels/response/VideoPicture.kt
+++ app/src/main/java/com/softyorch/taskapp/data/network/pexels/response/VideoPicture.kt
@@ -1,7 +1,9 @@
 package com.softyorch.taskapp.data.network.pexels.response
 
+import com.google.gson.annotations.SerializedName
+
 data class VideoPicture(
-    val id: Int,
-    val nr: Int,
-    val picture: String
+    @SerializedName("id") val id: Int,
+    @SerializedName("nr") val nr: Int,
+    @SerializedName("picture") val picture: String
 )
