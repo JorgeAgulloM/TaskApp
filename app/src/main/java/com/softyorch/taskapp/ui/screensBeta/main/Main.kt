95e67f3a8bdde34ea22742d992a3dc5722fb01eb
Jorge Agulló <agullojorge@gmail.com>
2022-10-17 16:52:19 +0200

    [WIP] MainScreenBeta has been added.

---
+++ app/src/main/java/com/softyorch/taskapp/ui/screensBeta/main/Main.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022. File developed by Jorge Agulló Martín for SoftYorch
+ */
+
+package com.softyorch.taskapp.ui.screensBeta.main
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreenBeta(navController: NavController){
+
+}
