97cc4890ddb9f06176ac4fc4a0c0370a9dc602b1
Jorge Agulló <agullojorge@gmail.com>
2022-10-17 18:54:38 +0200

    [WIP] Beta version working

--- app/src/main/java/com/softyorch/taskapp/ui/screensBeta/main/Main.kt
+++ app/src/main/java/com/softyorch/taskapp/ui/screensBeta/main/Main.kt
@@ -4,10 +4,78 @@
 
 package com.softyorch.taskapp.ui.screensBeta.main
 
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavController
+import com.softyorch.taskapp.ui.components.fabCustom.FABCustom
+import com.softyorch.taskapp.ui.screensBeta.main.components.BottomAppBarCustom
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreenBeta(navController: NavController){
+fun MainScreenBeta(navController: NavController) {
+    var check by remember { mutableStateOf(value = 0) }
 
+
+    Scaffold(
+        topBar = {},
+        floatingActionButton = { FABCustom() },
+        bottomBar = {
+            val items by remember {
+                mutableStateOf(
+                    value = listOf(
+                        BottomNavItem(
+                            true, "UnChecked Items", Icons.Rounded.CheckBoxOutlineBlank, 0
+                        ),
+                        BottomNavItem(
+                            false, "Checked Items", Icons.Rounded.CheckBox, 5
+                        ),
+                        BottomNavItem(
+                            false, "History", Icons.Rounded.History, 10
+                        )
+                    )
+                )
+            }
+            BottomAppBarCustom(
+                items = items,
+                navController = navController,
+                onItemClick = { itemButton ->
+                    items.forEach { item ->
+                        item.selected = (item.name == itemButton.name)
+                        item.badgeCount += 1
+                    }
+                    Log.d("ITEMS", "Items -> $items")
+                    if (check < 2) {
+                        check += 1
+                    } else {
+                        check = 0
+                    }
+                }
+            )
+        }
+    ) { Text(text = check.toString()) }
 }
+
+sealed class ItemNav() {
+    object Checked : ItemNav()
+    object UnChecked : ItemNav()
+    object History : ItemNav()
+}
+
+data class BottomNavItem(
+    var selected: Boolean,
+    val name: String,
+    val icon: ImageVector,
+    var badgeCount: Int = 0
+)
---
+++ app/src/main/java/com/softyorch/taskapp/ui/screensBeta/main/components/BottomAppBar.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2022. File developed by Jorge Agulló Martín for SoftYorch
+ */
+
+package com.softyorch.taskapp.ui.screensBeta.main.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.softyorch.taskapp.ui.screensBeta.main.BottomNavItem
+import com.softyorch.taskapp.utils.ELEVATION_DP
+import kotlinx.coroutines.selects.select
+
+
+@Composable
+fun BottomAppBarCustom(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+) {
+
+    BottomNavigation (
+        modifier = modifier,
+        backgroundColor = MaterialTheme.colorScheme.onBackground,
+        //cutoutShape = MaterialTheme.shapes.large,
+        elevation = ELEVATION_DP
+    ) {
+        items.forEach { item ->
+            BottomNavigationItem(
+                selected = item.selected,
+                onClick = { onItemClick(item) },
+                icon = {
+                    Column(
+                        modifier = Modifier.padding(vertical = 2.dp),
+                        horizontalAlignment = CenterHorizontally
+                    ) {
+                        if (item.badgeCount > 0) {
+                            BadgedBox(
+                                badge = {
+                                    Text(
+                                        text = item.badgeCount.toString(),
+                                        color = MaterialTheme.colorScheme.error
+                                    )
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+                            }
+                        } else {
+                            Icon(imageVector = item.icon, contentDescription = item.name)
+                        }
+                        if (item.selected) {
+                            Text(
+                                text = item.name,
+                                style = MaterialTheme.typography.labelSmall.copy(
+                                    textAlign = TextAlign.Center
+                                )
+                            )
+                        }
+                    }
+                },
+                selectedContentColor = MaterialTheme.colorScheme.primary,
+                unselectedContentColor = MaterialTheme.colorScheme.secondary
+            )
+        }
+    }
+}
