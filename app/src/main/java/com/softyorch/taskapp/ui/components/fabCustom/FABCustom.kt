cefc0b323965c77a96c6415b900376297f0fbfb4
Jorge Agulló <agullojorge@gmail.com>
2022-10-25 19:53:41 +0200

    [WIP] Beta version working. Future FAB and New Task

--- app/src/main/java/com/softyorch/taskapp/ui/components/fabCustom/FABCustom.kt
+++ app/src/main/java/com/softyorch/taskapp/ui/components/fabCustom/FABCustom.kt
@@ -1,35 +1,32 @@
 package com.softyorch.taskapp.ui.components.fabCustom
 
-import android.annotation.SuppressLint
-import androidx.compose.animation.*
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.animateIntSizeAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Description
+import androidx.compose.material.icons.rounded.Title
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.softyorch.taskapp.R
 import com.softyorch.taskapp.R.string.*
 import com.softyorch.taskapp.ui.components.ButtonCustom
+import com.softyorch.taskapp.ui.components.outlinedTextFieldCustom
 import com.softyorch.taskapp.ui.widgets.newTask.newTask
-import com.softyorch.taskapp.utils.ELEVATION_DP
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.softyorch.taskapp.utils.KEYBOARD_OPTIONS_CUSTOM
 
-@SuppressLint("CoroutineCreationDuringComposition")
 @ExperimentalMaterial3Api
 @Composable
 fun FABCustom() {
@@ -52,12 +49,6 @@ fun FABCustom() {
         )
     )
 
-    var open by remember { mutableStateOf(false) }
-    rememberCoroutineScope().launch {
-        delay(200)
-        open = true
-    }
-
     val height by animateIntAsState(
         targetValue = if (openDialog) calculateHeight else 55,
         animationSpec = tween(
@@ -67,24 +58,74 @@ fun FABCustom() {
     )
 
     val width by animateIntAsState(
-        targetValue = if (openDialog) calculateWidth else 155,
+        targetValue = if (openDialog) calculateWidth else 156,
         animationSpec = tween(
             durationMillis = 300,
             easing = FastOutSlowInEasing
         )
     )
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd) {
-        Box(
+    Box(
+        modifier = Modifier
+            .height(height.dp)
+            .width(width.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        Column(
             modifier = Modifier
                 .height(height.dp)
                 .width(width.dp)
-                .fillMaxSize()
                 .background(brush = sheetBrush, shape = MaterialTheme.shapes.large),
-            contentAlignment = Alignment.TopCenter
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
         ) {
-            ButtonCustom(text = "Close", true) {
-                openDialog = false
+            //Head
+
+            //Body
+            Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)) {
+                outlinedTextFieldCustom(
+                    text = "",
+                    label = stringResource(name_task),
+                    placeholder = stringResource(write_name),
+                    icon = Icons.Rounded.Title,
+                    contentDescription = stringResource(write_name),
+                    keyboardOptions = KEYBOARD_OPTIONS_CUSTOM,
+                    keyboardActions = KeyboardActions(),
+                    isError = false,
+                    isVisible = true,
+                    password = false,
+                    onTextFieldChanged = {
+
+                    }
+                )
+                outlinedTextFieldCustom(
+                    text = "",
+                    label = stringResource(task_description),
+                    placeholder = stringResource(write_description),
+                    icon = Icons.Rounded.Description,
+                    contentDescription = stringResource(write_description),
+                    keyboardOptions = KEYBOARD_OPTIONS_CUSTOM,
+                    keyboardActions = KeyboardActions(),
+                    isError = false,
+                    isVisible = true,
+                    password = false,
+                    onTextFieldChanged = {
+
+                    }
+                )
+            }
+            //Footer
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                ButtonCustom(text = "Save", true) {
+                    openDialog = false
+                }
+                ButtonCustom(text = "Close", true) {
+                    openDialog = false
+                }
             }
         }
 
@@ -103,7 +144,6 @@ fun FABCustom() {
             onClick = { openDialog = true }
         )
     }
-
 
 
 }
