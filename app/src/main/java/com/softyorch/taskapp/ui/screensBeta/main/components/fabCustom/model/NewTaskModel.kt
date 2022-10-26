4338a43aca4cbbdeb07c4c45c79aa525af18a097
Jorge Agulló <agullojorge@gmail.com>
2022-10-26 11:20:30 +0200

    [WIP] Beta version working. New vars added

--- app/src/main/java/com/softyorch/taskapp/ui/screensBeta/main/components/fabCustom/model/NewTaskModel.kt
+++ app/src/main/java/com/softyorch/taskapp/ui/screensBeta/main/components/fabCustom/model/NewTaskModel.kt
@@ -4,15 +4,13 @@
 
 package com.softyorch.taskapp.ui.screensBeta.main.components.fabCustom.model
 
-import com.softyorch.taskapp.ui.models.TaskModelUiMain
+import com.softyorch.taskapp.utils.emptyString
+import java.time.Instant
+import java.util.*
 
 data class NewTaskModel(
-    var title: String,
-    var description: String
+    var title: String = emptyString,
+    var description: String = emptyString,
+    var author: String = emptyString,
+    var entryDate: Date = Date.from(Instant.now())
 )
-
-fun TaskModelUiMain.mapToNewTaskModel(): NewTaskModel =
-    NewTaskModel(
-        title = this.title,
-        description = this.description
-    )
